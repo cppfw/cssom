465bd3380de6d191aebbeb17824be7848bcac50c
Ivan Gagis <igagis@gmail.com>
2020-06-02 01:15:02 +0300

    test improved

--- tests/selectors/main.cpp
+++ tests/selectors/main.cpp
@@ -18,6 +18,12 @@ int main(int argc, char** argv){
 				stroke: blue;
 				stroke-width: 3
 			}
+			circle.myGreen {
+				stroke: #006600; fill: #00cc00;
+			}
+			circle.myRed {
+				stroke: #670000; fill: #cc0000;
+			}
 		)qwertyuiop";
 
 		auto pntim = &property_name_to_id_map;
@@ -40,19 +46,33 @@ int main(int argc, char** argv){
 		typedef utki::tree<dom_node> node;
 		node::container_type dom{
 			node(dom_node("body"), {
-				node(dom_node("rect"))
+				node(dom_node("rect")),
+				node(dom_node("circle", std::string(), {"myGreen"}))
 			})
 		};
 
-		crawler cr(dom, {0, 0});
-		
-		auto qr = css_dom.get_property_value(cr, uint32_t(property_id::stroke));
-		ASSERT_ALWAYS(qr.value);
+		{
+			crawler cr(dom, {0, 0});
+			
+			auto qr = css_dom.get_property_value(cr, uint32_t(property_id::stroke));
+			ASSERT_ALWAYS(qr.value);
 
-		ASSERT_ALWAYS(dynamic_cast<const property_value*>(qr.value))
+			ASSERT_ALWAYS(dynamic_cast<const property_value*>(qr.value))
 
-		auto pv = static_cast<const property_value*>(qr.value);
-		ASSERT_ALWAYS(pv->value == "blue")
+			auto pv = static_cast<const property_value*>(qr.value);
+			ASSERT_ALWAYS(pv->value == "blue")
+		}
+		{
+			crawler cr(dom, {0, 1});
+			
+			auto qr = css_dom.get_property_value(cr, uint32_t(property_id::stroke));
+			ASSERT_ALWAYS(qr.value);
+
+			ASSERT_ALWAYS(dynamic_cast<const property_value*>(qr.value))
+
+			auto pv = static_cast<const property_value*>(qr.value);
+			ASSERT_ALWAYS(pv->value == "#006600")
+		}
 	}
 
 	return 0;
