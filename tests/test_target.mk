19ad1f347027b3254a4210fa0159ee4eeca14590
Ivan Gagis <igagis@gmail.com>
2021-04-04 14:52:36 +0300

    fix msys2 build

--- tests/test_target.mk
+++ tests/test_target.mk
@@ -1,6 +1,6 @@
 ifeq ($(os),windows)
     # to avoid /C converted to C:\ need to escape it as //C
-    this_test_cmd := (cd $(d)$(this_out_dir) && cmd //C 'set PATH=../../../src/out/$(c);../../harness/out/$(c);%PATH% && $$(notdir $$^)')
+    this_test_cmd := (cd $(d)$(this_out_dir) && cmd //C 'set PATH=../../../../src/out/$(c);../../../harness/out/$(c);%PATH% && $$(notdir $$^)')
 else ifeq ($(os),macosx)
     this_test_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src/out/$(c):../harness/out/$(c) $(this_out_dir)/$$(notdir $$^))
 else ifeq ($(os),linux)
@@ -19,7 +19,7 @@ $(eval $(this_rule))
 
 ifeq ($(os),windows)
     # to avoid /C converted to C:\ need to escape it as //C
-    this_gdb_cmd := (cd $(d)$(this_out_dir) && cmd //C 'set PATH=../../../src/out/$(c);../../harness/out/$(c);%PATH% && gdb $$(notdir $$^)')
+    this_gdb_cmd := (cd $(d)$(this_out_dir) && cmd //C 'set PATH=../../../../src/out/$(c);../../../harness/out/$(c);%PATH% && gdb $$(notdir $$^)')
 else ifeq ($(os),macosx)
     this_gdb_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src/out/$(c):../harness/out/$(c) gdb $(this_out_dir)/$$(notdir $$^))
 else ifeq ($(os),linux)
